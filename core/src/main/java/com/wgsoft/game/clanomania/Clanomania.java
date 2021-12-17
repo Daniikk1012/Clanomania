350b50b2749bd612f514e5055bdc6a829acc8982
Daniikk1012 <daniyarsar@gmail.com>
2021-12-17 22:07:14 +0600

    Reogranize assets structure

--- core/src/main/java/com/wgsoft/game/clanomania/Clanomania.java
+++ core/src/main/java/com/wgsoft/game/clanomania/Clanomania.java
@@ -2,18 +2,20 @@ package com.wgsoft.game.clanomania;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public final class Clanomania extends ApplicationAdapter {
     private SpriteBatch spriteBatch;
-    private Texture texture;
+
+    private Skin skin;
 
     @Override
     public void create() {
         spriteBatch = new SpriteBatch();
-        texture = new Texture(Gdx.files.internal("texture.png"));
+
+        skin = new Skin(Gdx.files.internal("img/skin.json"));
     }
 
     @Override
@@ -21,13 +23,14 @@ public final class Clanomania extends ApplicationAdapter {
         ScreenUtils.clear(1f, 0f, 0f, 1f);
 
         spriteBatch.begin();
-        spriteBatch.draw(texture, 0f, 0f);
+        spriteBatch.draw(skin.getRegion("texture"), 0f, 0f);
         spriteBatch.end();
     }
 
     @Override
     public void dispose() {
         spriteBatch.dispose();
-        texture.dispose();
+
+        skin.dispose();
     }
 }
